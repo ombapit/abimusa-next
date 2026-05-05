d8759f5f2c1d5757aa0741685899d41d91d37bba
David Suwandi <davidsuwandi@gmail.com>
2026-05-05 16:01:56 +0700

    fix: docker

--- src/app/jadwal/dashboard/layout.tsx
+++ src/app/jadwal/dashboard/layout.tsx
@@ -1,17 +1,5 @@
 import type { Metadata } from "next";
-
-import { Geist, Geist_Mono } from "next/font/google";
 import "@/app/globals.css";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 export const metadata: Metadata = {
   title: "Masjid Abi Musa Al-Asy'ari",
@@ -26,10 +14,8 @@ export default function DashboardLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >        
-        {children}    
+      <body className="antialiased">
+        {children}
       </body>
     </html>
   );
